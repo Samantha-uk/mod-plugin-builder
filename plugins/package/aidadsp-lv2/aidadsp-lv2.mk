b017319a34681096dec6bd10f8f5d9238a704dd6
falkTX <falktx@falktx.com>
2024-01-31 15:38:41 +0100

    Update aidadsp-lv2
    Signed-off-by: falkTX <falktx@falktx.com>

--- plugins/package/aidadsp-lv2/aidadsp-lv2.mk
+++ plugins/package/aidadsp-lv2/aidadsp-lv2.mk
@@ -4,7 +4,7 @@
 #
 ######################################
 
-AIDADSP_LV2_VERSION = bb2751b52d50c9b926780dc4063be2ca79dc775b
+AIDADSP_LV2_VERSION = 5fef7820012bc4dc5fac1a0d8a542ea60a0338b7
 AIDADSP_LV2_SITE = https://github.com/AidaDSP/aidadsp-lv2.git
 AIDADSP_LV2_SITE_METHOD = git
 AIDADSP_LV2_BUNDLES = rt-neural-generic.lv2
